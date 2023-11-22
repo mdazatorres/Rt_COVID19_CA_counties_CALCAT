f7d8568c996f8a0c66a8babec612076bec9692a5
marialuisa <mdazatorres@cimat.mx>
2023-11-22 09:26:35 -0800

    Update Rt

--- estimated_Rts.py
+++ estimated_Rts.py
@@ -112,7 +112,7 @@ def save_Rt_all(county, all):
 
     else:
         k = math.floor((current_date - date_one).days / 7)
-        res=0 # uncomented when run in a different day than thursday
+        #res=0 # uncomented when run in a different day than thursday
         if res==0:
             #current_date = pd.to_datetime(datetime.date.today())
             df_Rt = save_Rt_csv(county=county, per=per+k, forcast=res, all=False)
